0b9640f97289e3a508d8cb578d0263244ba1e473
Simon Roeser <simonroeser@oakland.edu>
2026-03-28 13:25:57 -0400

    Add files via upload

---
+++ RapidRespond/RapidRespond/RapidRespondApp.swift
@@ -0,0 +1,32 @@
+//
+//  RapidRespondApp.swift
+//  RapidRespond
+//
+//  Created by simon roeser on 3/28/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RapidRespondApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RapidRespond/RapidRespondTests/RapidRespondTests.swift
@@ -0,0 +1,19 @@
+//
+//  RapidRespondTests.swift
+//  RapidRespondTests
+//
+//  Created by simon roeser on 3/28/26.
+//
+
+import Testing
+@testable import RapidRespond
+
+struct RapidRespondTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
